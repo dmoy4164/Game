3dc966eaefc7556317b4441be320aebacc668243
dmoy4164 <donniem@nycstudents.net>
2022-12-06 20:15:57 -0500

    updated things

--- src/Game.java
+++ src/Game.java
@@ -5,11 +5,11 @@ public class Game {
     /**
      * Represents the amount of health the enemy has. By default, the first enemy has 200 health.
      */
-    public double ehealth = 200;
+    public double eHealth = 200;
     /**
      * Represents the amount of health the user has. By default, the user has 100 health.
      */
-    public double uhealth = 100;
+    public double uHealth = 100;
     /**
      * Stores the user's inputted name.
      */
@@ -17,7 +17,7 @@ public class Game {
     /**
      * Stores the enemy's name. By default, the first enemy is 'Elias Wu Feng'.
      */
-    public String ename = ("Elias Wu Feng");
+    public String eName = ("Elias Wu Feng");
     /**
      * Stores the user's choice of weapon. By default, it is 'fists'.
      */
@@ -25,11 +25,11 @@ public class Game {
     /**
      * Stores the base damage of the weapon the user chose. By default, it is 5 damage with 'fists'.
      */
-    private double udamage = 5;
+    private double uDamage = 5;
     /**
      * Stores the base damage of the enemy. By default, it is 0.
      */
-    private double edamage = 0;
+    private double eDamage = 0;
     /**
      * Stores the user's choice of action.
      */
@@ -42,40 +42,44 @@ public class Game {
      * Stores the total amount of water the user has. By default, it is 5.
      */
     public int water = 5;
+    /**
+     * Determines which fight the user is in.
+     */
+    public int fight = 1;
 
     /**
   * Constructs the user through their name and choice of weapon.
-  * @param nam Sets the user's name to variable name.
-  * @param weap Sets the user's weapon to variable weapon.
+  * @param nameA Sets the user's name to variable name.
+  * @param weaponA Sets the user's weapon to variable weapon.
      */
-    public Game (String nam, String weap){
-        name = nam;
-        weapon = weap;
+    public Game (String nameA, String weaponA){
+        name = nameA;
+        weapon = weaponA;
         if (weapon.equalsIgnoreCase("sword")){
-            udamage = 20;
+            uDamage = 20;
         }
         if (weapon.equalsIgnoreCase("mace")){
-            udamage = 30;
+            uDamage = 30;
         }
         if (weapon.equalsIgnoreCase("keyboard")){
-            udamage = 26;
+            uDamage = 26;
         }
         if (weapon.equalsIgnoreCase("apcsa textbook")){
-            udamage = 40;
+            uDamage = 40;
         }
     }
     /**
      * Constructs the user through their name and defaulting their weapon of choice to 'fists'. If certain special names are chosen, unreasonable amount of stats are given.
-     * @param namee Sets the user's name to variable name.
+     * @param nameB Sets the user's name to variable name.
      */
-    public Game (String namee){
-        name = namee;
+    public Game (String nameB){
+        name = nameB;
         if (name.equalsIgnoreCase("saitama")){
-            udamage = 200;
+            uDamage = 200;
         }
         if (name.equalsIgnoreCase("donnie")){
-            uhealth = 54321;
-            udamage = 12345;
+            uHealth = 54321;
+            uDamage = 12345;
         }
     }
     /**
@@ -88,46 +92,45 @@ public class Game {
 
     /**
      * Determines the user's turn and their actions and carries them out. Also contains the amount of damage the user and enemy will do.
-     * @param abc Decides the action the user uses.
-     * @param fight Decides which fight the user is in, which also determines what actions are available and enemy's damage.
+     * @param action Decides the action the user uses.
      * @return String - Commentary on the action the user used and the effect it had on the enemy.
      */
-    public String userTurn (String abc, int fight){
+    public String userTurn (String action){
         //add item usage//
         if (fight == 1){
-            edamage = Math.random()*15+1;
+            eDamage = Math.random()*15+1;
         } else if (fight == 2){
-            edamage = Math.random()*20+1;
+            eDamage = Math.random()*20+1;
         } else {
-            edamage = Integer.MIN_VALUE;
+            eDamage = Integer.MIN_VALUE;
         }
         double halfChance = Math.random();
         if (halfChance>=0.5){
-            udamage = udamage + (Math.random()*(10)+1);
+            uDamage = uDamage + (Math.random()*(10)+1);
         } else {
-            udamage = udamage - (Math.random()*(2)+1);
+            uDamage = uDamage - (Math.random()*(2)+1);
         }
 
-        if (abc.equalsIgnoreCase("attack")){
-            ehealth = ehealth - udamage;
+        if (action.equalsIgnoreCase("attack")){
+            eHealth = eHealth - uDamage;
 
-            return("You did " + udamage + " damage!");
+            return("You did " + uDamage + " damage!");
         }
-        if (abc.equalsIgnoreCase("flee")){
+        if (action.equalsIgnoreCase("flee")){
             return("no.");
         }
-        if (abc.equalsIgnoreCase("block")){
-            edamage = edamage/2;
+        if (action.equalsIgnoreCase("block")){
+            eDamage = eDamage /2;
             return("You prepare for an incoming attack.");
         }
-        if (abc.equalsIgnoreCase("use water")){
+        if (action.equalsIgnoreCase("use water")){
             return(itemUse("water"));
         }
-        if (abc.equalsIgnoreCase("use bread")){
+        if (action.equalsIgnoreCase("use bread")){
             return(itemUse("bread"));
         }
-        if (abc.equals("System.exit(0);")){
-            ehealth = 0;
+        if (action.equals("System.exit(0);")){
+            eHealth = 0;
             return("\n" + "i-i-impossible! how?!" + "\n" +"Process finished with exit code 0" + "\n");
 
         }
@@ -135,26 +138,25 @@ public class Game {
     }
     /**
      * Determines the enemy's actions and carries them out. Deals the damage to the user.
-     * @param enemy Decides which enemy the user is fighting.
-     * @return String - Commentary on the action the enemy used and the effect it had on the user.
+     * Prints commentary on the action the enemy used and the effect it had on the user.
      */
-    public void enemyTurn(int enemy){
+    public void enemyTurn(){
         int output = (int) (Math.random()*(4)+1);
-        if (enemy == 1){
+        if (fight == 1){
             if (output == 1){
-                System.out.println(ename + " tackles you.");
+                System.out.println(eName + " tackles you.");
             }
             if (output == 2){
-                System.out.println(ename + " punches you.");
+                System.out.println(eName + " punches you.");
             }
             if (output == 3){
-                System.out.println(ename + " kicks you.");
+                System.out.println(eName + " kicks you.");
             }
             if (output == 4){
-                System.out.println(ename + " headbutts you.");
+                System.out.println(eName + " headbutts you.");
             }
 
-        } else if (enemy == 2){
+        } else if (fight == 2){
             if (output == 1){
                 System.out.println("Andy throws his backpack at you.");
             }
@@ -169,20 +171,20 @@ public class Game {
             }
         } else{
             if (output == 1){
-                System.out.println(ename + " compiles you to git.");
+                System.out.println(eName + " compiles you to git.");
             }
             if (output == 2){
-                System.out.println(ename + " sends you to github.");
+                System.out.println(eName + " sends you to github.");
             }
             if (output == 3){
-                System.out.println(ename + " runs your process.");
+                System.out.println(eName + " runs your process.");
             }
             if (output == 4){
-                System.out.println(ename + " throws an error at you.");
+                System.out.println(eName + " throws an error at you.");
             }
         }
-        uhealth = uhealth-edamage;
-        System.out.println(edamage + " damage taken!");
+        uHealth = uHealth - eDamage;
+        System.out.println(eDamage + " damage taken!");
     }
     /**
      * Allows for the user to use items.
@@ -192,7 +194,7 @@ public class Game {
     public String itemUse(String item){
         if (item.equalsIgnoreCase("water")){
             if (water>0){
-                uhealth = 100;
+                uHealth = 100;
                 return ("You drank water. HP restored to full.");
             } else {
                 return ("You have no water!");
@@ -201,9 +203,9 @@ public class Game {
         water= water-1;
         if (item.equalsIgnoreCase("bread")){
             if (bread>0){
-                ehealth = ehealth-75;
-                return ("You threw your bread at " + ename + ". They take 75 damage.");
-            } else{
+                eHealth = eHealth -75;
+                return ("You threw your bread at " + eName + ". They take 75 damage.");
+            } else {
                 return ("You have no bread!");
             }
         }
@@ -213,21 +215,21 @@ public class Game {
     /**
      * Organizes large blocks of text into one place to be called.
      * @param num Allows for selection of which section of text to print.
-     * @return void - Prints out lots of text.
+     * Prints out lots of text.
      */
     public void talk (int num){
         if (num == 1){
             System.out.println();
-            System.out.println(ename + ": " + ehealth + " HP");
-            System.out.println("You: " + uhealth + " HP");
+            System.out.println(eName + ": " + eHealth + " HP");
+            System.out.println("You: " + uHealth + " HP");
             System.out.println("What do you want to do? attack/flee/block");
         }
         if (num == 2){
-            System.out.println("now go defeat the evil minion, " +ename + "\n"+"\n");
+            System.out.println("now go defeat the evil minion, " + eName + "\n"+"\n");
             System.out.println("------------------" + "\n" + "ELIAS WU FENG INCOMING!!" + "\n"+"------------------"+"\n");
         }
         if (num == 3){
-            System.out.println("\n"+ename+": " + name + ", remember our words...system dot exit...");
+            System.out.println("\n"+ eName +": " + name + ", remember our words...system dot exit...");
             System.out.println();
             System.out.println("Congrats, " + name + ". You've defeated the ruthless Elias Wu Feng. Here, drink some water.");
             System.out.println(itemUse("water"));
@@ -241,12 +243,12 @@ public class Game {
         }
         if (num == 5){
             System.out.println();
-            System.out.println(ename + ": " + ehealth + " HP");
-            System.out.println("You: " + uhealth + " HP");
+            System.out.println(eName + ": " + eHealth + " HP");
+            System.out.println("You: " + uHealth + " HP");
             System.out.println("What do you want to do? attack/flee/block/use item");
         }
         if (num == 0){
-            System.out.println("\n"+ename + ": kghhgjhbfhdgmakhfgjfdk...zero...");
+            System.out.println("\n"+ eName + ": kghhgjhbfhdgmakhfgjfdk...zero...");
             System.out.println("Congrats, " + name + ". You've defeated the mighty Andy Liang. Now this may be unexpected, ");
             System.out.println("but it's time for me to rid of you. I must thank you for freeing me of all constraints. ");
             System.out.println("I'll play with you for a bit...");
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        /*
+         * Begins the game.
+         */
         Scanner scan = new Scanner(System.in);
         System.out.println( "hi. an evil guy has been terrorizing the APCSA classroom for months. we desperately need your help." + "\n"+
                 "his name is andy liang and you must defeat him. you have no choice in the matter. practice first on his evil minion, elias wu feng." + "\n"+
@@ -9,7 +12,6 @@ public class Main {
         Game start = new Game(a);
         System.out.println("ok "+a+", do you want a weapon? (yes/no)");
         String c = scan.nextLine();
-
         if (c.equals("yes")){
             System.out.println("what weapon? i have the following: sword, mace, keyboard, apcsa textbook");
             String b = scan.nextLine();
@@ -17,56 +19,65 @@ public class Main {
         } else {
             System.out.println("wow ok. confident aren't you?");
         }
+        /*
+         * Begins first part.
+         */
         System.out.println("\n" + start + "\n");
         start.talk(2);
-        while (start.uhealth>0 && start.ehealth>0){
+        while (start.uHealth >0 && start.eHealth >0){
             start.talk(1);
             start.action = scan.nextLine();
-            System.out.println(start.userTurn(start.action,1));
-            start.enemyTurn(1);
+            System.out.println(start.userTurn(start.action));
+            start.enemyTurn();
         }
-        if (start.uhealth<=0){
+        if (start.uHealth <=0){
             System.out.println("\n"+"what a pity. you died. ill have to find someone else to replace you.");
             System.exit(0);
         }
-        if (start.ehealth<=0){
+        if (start.eHealth <=0){
             start.talk(3);
-            start.ehealth = 581;
-            start.ename = ("Andy Liang");
+            start.eHealth = 581;
+            start.eName = ("Andy Liang");
         }
-        //start 2nd part here//
+        /*
+         * Begins second part.
+         */
         start.talk(4);
-        while (start.uhealth>0 && start.ehealth>0){
+        while (start.uHealth >0 && start.eHealth >0){
             start.talk(5);
             start.action = scan.nextLine();
-            System.out.println(start.userTurn(start.action,2));
-            start.enemyTurn(2);
+            System.out.println(start.userTurn(start.action));
+            start.enemyTurn();
         }
-        if (start.uhealth<=0) {
+        if (start.uHealth <=0) {
             System.out.println("\n" + "what a pity. you died. ill have to find someone else to replace you.");
             System.exit(0);
         }
-        if (start.ehealth<=0){
+        /*
+         * Begins third part.
+         */
+        if (start.eHealth <=0){
             start.talk(0);
-            start.ehealth = Integer.MAX_VALUE;
-            start.ename = ("IntelliJ IDEA Community Edition 2022.2.2");
+            start.eHealth = Integer.MAX_VALUE;
+            start.eName = ("IntelliJ IDEA Community Edition 2022.2.2");
         }
-        while (start.uhealth>0 && start.ehealth>0){
+        while (start.uHealth >0 && start.eHealth >0){
             start.talk(5);
             start.action = scan.nextLine();
-            System.out.println(start.userTurn(start.action,2));
-            if (start.ehealth>0){
-                start.enemyTurn(3);
+            System.out.println(start.userTurn(start.action));
+            if (start.eHealth >0){
+                start.enemyTurn();
             }
         }
-        if (start.uhealth<=0){
+        if (start.uHealth <=0){
             System.out.println("I pity thee.");
             System.exit(0);
         }
-        if (start.ehealth<=0){
+        /*
+         * Ends.
+         */
+        if (start.eHealth <=0){
             start.talk(6);
         }
-
-
     }
 }
